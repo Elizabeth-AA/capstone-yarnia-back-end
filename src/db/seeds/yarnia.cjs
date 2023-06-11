82d7e026e93e18a824fe2d471ba7ea8aa3ca6a19
Elizabeth-AA <elizabeth2awesome@gmail.com>
2023-06-10 23:11:21 -0700

    feat: updated migration and seed

--- src/db/seeds/yarnia.cjs
+++ src/db/seeds/yarnia.cjs
@@ -8,15 +8,15 @@ exports.seed = function (knex) {
       return knex('users').insert(userData);
     })
     .then(() => {
-      return knex('users_yarn').del();
-    })
-    .then(() => {
-      return knex('users_yarn').insert(userYarnData);
-    })
-    .then(() => {
       return knex('yarn').del();
     })
     .then(() => {
       return knex('yarn').insert(yarnData);
+    })
+    .then(() => {
+      return knex('users_yarn').del();
+    })
+    .then(() => {
+      return knex('users_yarn').insert(userYarnData);
     })
 }
